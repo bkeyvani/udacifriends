3108dbce2a3ed569da642ce2993cdd9c4d2afec3
Babak Keyvani <bkeyvani@gatech.edu>
2015-01-19 16:18:29 -0500

    added friends, messages, register views

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -8,6 +8,15 @@ app.config(['$routeProvider', function($routeProvider) {
     when('/login', {
       templateUrl: 'views/login.html'
   }).
+    when('/register', {
+      templateUrl: 'views/register.html'
+  }).
+    when('/friends', {
+      templateUrl: 'views/friends.html'
+  }).
+    when('/messages', {
+      templateUrl: 'views/messages.html'
+  }).
   otherwise({
     redirectTo: '/'
   });
