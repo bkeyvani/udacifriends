c69b46bafe24df3a29bf67eb957694dfe8e489b2
Babak Keyvani <bkeyvani@gatech.edu>
2015-01-31 00:21:13 -0500

    added basic friendship functionality

--- src/components/scripts/controllers/friends.js
+++ src/components/scripts/controllers/friends.js
@@ -1,9 +1,39 @@
 app.controller('FriendsController',
-  function($scope, $firebase) {
+  ['$scope', '$firebase', 'FIREBASE_URL', 'currentAuth', 'User',
+  function($scope, $firebase, FIREBASE_URL, currentAuth) {
 
-    var fb = new Firebase('https://udacifriendz.firebaseio.com/friends');
-    var friends = $firebase(fb);
+    var currentUser = currentAuth.uid;
+    var ref = new Firebase(FIREBASE_URL).child('users').child(currentUser).child('friends');
+    var friends = $firebase(ref);
+    var friendsObj = friends.$asObject();
+    var friendsArray = friends.$asArray();
 
-    $scope.friends = friends.$asObject();
+    friendsObj.$loaded().then(function(data) {
+        $scope.friends = friendsObj;
+      }); // friends Object Loaded
+
+      friendsArray.$loaded().then(function(data) {
+        $scope.friendsCnt = friendsArray.length;
+      }); // friends Array Loaded
+
+      friendsArray.$watch(function(event) {
+        $scope.friendsCnt = friendsArray.length;
+      });
+
+      $scope.addFriend = function() {
+        friendId = $scope.friendName;
+        friends.$set(friendId , {
+          fullName: 'Your "best" Pal',
+          date: Firebase.ServerValue.TIMESTAMP
+        })
+          .then(function(ref) {
+            console.log(ref);
+            $scope.friendName = '';
+          });
+      } // addFriend
+
+      $scope.deleteFriend = function(key) {
+        friends.$remove(key);
+      } //deleteFriend
   }
-); // FriendsController
+]); // FriendsController
