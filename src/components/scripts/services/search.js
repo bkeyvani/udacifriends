146ace6a8fdd3990712f61e8403feb3d7609f555
Babak Keyvani <bkeyvani@gatech.edu>
2015-02-18 20:26:03 -0500

    added search factory to be used from any controller in the app
    need to add a search custom directive

---
+++ src/components/scripts/services/search.js
@@ -0,0 +1,18 @@
+// Search Factory
+
+app.factory('SearchFctr', [function() {
+  return function(user, query) {
+    if (query !== '') {
+      var firstName, lastName, fullName;
+
+      firstName = user.firstname.toLowerCase();
+      lastName = user.lastname.toLowerCase();
+      fullName = firstName + ' ' + lastName;
+      query = query.toLowerCase();
+
+      return !!((firstName.indexOf(query || '') !== -1 ||
+                 lastName.indexOf(query || '') !== -1 ||
+                 fullName.indexOf(query || '') !== -1));
+    }
+  };
+}]);
