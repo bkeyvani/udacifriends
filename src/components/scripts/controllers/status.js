06c73ce0a7e66e29a54b02875bb79a02e0619f4d
Babak Keyvani <bkeyvani@gatech.edu>
2015-02-20 19:55:17 -0500

    removed active state from menu

--- src/components/scripts/controllers/status.js
+++ src/components/scripts/controllers/status.js
@@ -1,23 +1,25 @@
 app.controller('StatusController', ['$scope', '$rootScope', '$location', '$firebase', 'AuthFactory', 'FIREBASE_URL',
   function($scope, $rootScope, $location, $firebase, AuthFactory, FIREBASE_URL) {
 
-  $scope.logout = function() {
-    AuthFactory.logout();
-  }; // logout
+    $rootScope.location = $location;
+    $scope.logout = function() {
+      AuthFactory.logout();
+    }; // logout
 
-  $rootScope.authObj = AuthFactory.authObj;
+    $rootScope.authObj = AuthFactory.authObj;
 
-  $rootScope.authObj.$onAuth(function(authData) {
-    if (authData) {
-      var ref = new Firebase(FIREBASE_URL + 'users/' + authData.uid)
-      var user = $firebase(ref).$asObject();
+    $rootScope.authObj.$onAuth(function(authData) {
+      if (authData) {
+        var ref = new Firebase(FIREBASE_URL + 'users/' + authData.uid)
+        var user = $firebase(ref).$asObject();
 
-      user.$loaded().then(function() {
-        $rootScope.currentUser = user;
-      });
-    } else {
-        $rootScope.currentUser = null;
-        $location.path('/logout'); // TODO: add a separate logout page (?)
-    }
-  }); // authObj $onAuth event
-}]);
+        user.$loaded().then(function() {
+          $rootScope.currentUser = user;
+        });
+      } else {
+          $rootScope.currentUser = null;
+          $location.path('/logout'); // TODO: add a separate logout page (?)
+      }
+    }); // authObj $onAuth event
+  }
+]);
